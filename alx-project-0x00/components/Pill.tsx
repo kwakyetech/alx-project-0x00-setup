6448bd86c244ae980376258c4388acb409e51635
kwakyetech <kwakyeoforiprince@gmail.com>
2025-05-27 19:38:49 +0000

    define PillProps interface and update Pill component to accept dynamic title prop

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,7 +1,8 @@
-const Pill: React.FC = () => {
+import { PillProps } from "@/interfaces";
+const Pill: React.FC<PillProps> = ({ title }) => {
   return (
     <div className=" flex justify-center  items-center bg-[#333333] px-3 w-auto h-[27px] rounded-full">
-      <p className=" text-sm ">Title</p>
+      <p className=" text-sm ">{title}</p>
     </div>
   )
 }
