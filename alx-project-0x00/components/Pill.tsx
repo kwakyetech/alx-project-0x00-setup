74e13e97f805cb55b23441d12f981a235f5b9721
kwakyetech <kwakyeoforiprince@gmail.com>
2025-05-27 19:18:56 +0000

    add reusable Card component with image, rating, and property details

---
+++ alx-project-0x00/components/Pill.tsx
@@ -0,0 +1,9 @@
+const Pill: React.FC = () => {
+  return (
+    <div className=" flex justify-center  items-center bg-[#333333] px-3 w-auto h-[27px] rounded-full">
+      <p className=" text-sm ">Title</p>
+    </div>
+  )
+}
+
+export default Pill;
