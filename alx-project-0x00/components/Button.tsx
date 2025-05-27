13848a79c89c936e17e78e8677af6b94fdc82b06
kwakyetech <kwakyeoforiprince@gmail.com>
2025-05-27 19:57:19 +0000

    create Button component with dynamic title and custom styles via props; render various sizes and shapes on landing page

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ButtonProps } from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({ title, styles }) => {
+  return (
+    <button
+      className={`bg-blue-500 text-white font-semibold px-4 py-2 ${styles}`}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default Button;
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,9 +1,16 @@
 import Card from '@/components/Card'
+import Button from "@/components/Button";
 const Landing: React.FC =  () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
+      <div className="mt-8 flex flex-col gap-4">
+  <h2 className="text-lg font-bold">Reusable Buttons</h2>
+  <Button title="Small Rounded-sm" styles="text-sm w-1/5 rounded-sm" />
+  <Button title="Medium Rounded-md" styles="text-base w-1/5 rounded-md" />
+  <Button title="Large Rounded-full" styles="text-lg w-1/5 rounded-full" />
+</div>
     </div>
   )
 }
