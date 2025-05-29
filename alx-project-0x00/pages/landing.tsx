dac3ce1ead654c978124ea841c41bc9e42d3583f
kwakyetech <kwakyeoforiprince@gmail.com>
2025-05-29 18:00:12 +0000

    modify landing page

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -5,10 +5,13 @@ const Landing: React.FC =  () => {
     <div>
         <h1 className=" text-xl font-extralight">Landing Page</h1>
         <Card />
-        <Button title="Contact" styles="rounded-sm" />
-        <Button title="Connect" styles="rounded-md" />
-        <Button title="Buy Now" styles="rounded-lg" />
-        <Button title="Buy Now" styles="rounded-full" />
+        <div className="flex gap-4 mt-4">
+          <Button title="Contact" styles="rounded-sm" />
+          <Button title="Connect" styles="rounded-md" />
+          <Button title="Buy Now" styles="rounded-lg" />
+          <Button title="Click Me" styles="rounded-full" />
+        </div>
+        
     </div>
   )
 }
