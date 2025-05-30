4208073a3ae3091e30b13f5ce6944b80c318a9cf
kwakyetech <kwakyeoforiprince@gmail.com>
2025-05-30 15:41:58 +0000

    implement UserModal component with dynamic form and nested input handling

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -48,3 +48,55 @@ export interface PostModalProps {
   onClose: () => void;
   onSubmit: (post: PostData) => void;
 }
+
+
+export interface UserModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onAddUser: (user: UserProps) => void;
+}
+
+export interface UserProps {
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+export interface UserData {
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
